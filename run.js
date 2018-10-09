93084635d4b6accd3beb4aad5307d46db4c08214
yenesey <denesey@gmail.com>
2018-10-09 16:17:42 +0300

    fixx

--- run.js
+++ run.js
@@ -3,6 +3,9 @@ const times = 50 // -по заданию
 const cluster = require('cluster')
 const mongoose = require('mongoose')
 const chalk   = require('chalk')
+const cyan = (text) => chalk.cyan.bold(text)
+const yellow = (text) => chalk.yellow.bold(text)
+const log = console.log
 
 /*
 	Соединяемся с базой, создаем схему и коллекцию
@@ -16,29 +19,28 @@ const Doc = mongoose.model('doc', Schema)
 	для теста над только что созданным документом
 */
 if (cluster.isMaster) {
-  console.log(chalk.cyan.bold(`Master ${process.pid} is running`))
-
-  console.log(chalk.cyan.bold('Creating document item...'))
+	log(cyan(`Master ${process.pid} is running`))
+	log(cyan('Creating document item...'))
 	Doc.create({ counter: 0 }).then(doc=>{
-		console.log(chalk.cyan.bold('Done:') + chalk.yellow.bold(doc))
-  	console.log(chalk.cyan.bold('Creating workers...'))
-  	var _id = doc._id
-  	for (let i = 0; i < numWorkers; i++) 
-  		cluster.fork({_id : _id, inc : i % 2 === 0?1:-1 })
+		log(cyan('Done:') + yellow(doc))
+		log(cyan('Creating workers...'))
+		let _id = doc._id
+		for (let i = 0; i < numWorkers; i++) 
+			cluster.fork({_id : _id, inc : i % 2 === 0?1:-1 })
 
-  	cluster.on('exit', (worker, code, signal) => {
-	  	//тут отлавливаем завершение работы дочерних процессов
-			 console.log(`Worker ${worker.process.pid} done`)
+		cluster.on('exit', (worker, code, signal) => {
+			//тут отлавливаем завершение работы дочерних процессов
+			 log(cyan(`Worker ${worker.process.pid} done`))
 
- 	  	//если больше нет дочерних, пора вывести результаты
-  		if (Object.keys(cluster.workers).length === 0){ 
-  			console.log(chalk.cyan.bold('Reloading document...'))
-	 	  	Doc.findOne({_id}).then(doc=>{
- 	  			console.log(chalk.cyan.bold('Done:') + chalk.yellow.bold(doc))
-  				process.exit(0)
+			//если больше нет дочерних, пора вывести результаты
+			if (Object.keys(cluster.workers).length === 0){ 
+				log(cyan('Reloading document...'))
+				Doc.findOne({_id}).then(doc=>{
+					log(cyan('Done:') + yellow(doc))
+					process.exit(0)
 				}).catch(console.error)
 			}	
-  	})
+		})
 
 	})
 	.catch(console.error)
@@ -51,13 +53,13 @@ if (cluster.isMaster) {
 		console.log('не передан _id и/или счетчик')
 		process.exit(0)	
 	}
-	var counter = parseInt(process.env.inc)
-	var _id = process.env._id
+	let counter = parseInt(process.env.inc)
+	let _id = process.env._id
 
-	console.log(`Worker ${process.pid} started task on _id = ${process.env._id} : inc ${counter} about ${times} times`)
+	log(`Worker ${process.pid} started task on _id = ${process.env._id} : inc ${counter} about ${times} times`)
 		
 	var ops = []
-	for (var i = 0; i < times; i++) 
+	for (let i = 0; i < times; i++) 
 		ops[i] = Doc.updateOne({ _id }, {$inc : { counter }} ).exec()
 	
 	Promise.all(ops)
@@ -66,5 +68,5 @@ if (cluster.isMaster) {
 		console.error(err)
 		process.exit(0)
 	})
-  
+	
 }
